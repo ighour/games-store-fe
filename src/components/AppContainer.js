b3e6a5a7e041de7ba1f2298ee8c85ee949a87f9f
Ighour <ti.celio.rodrigues@gmail.com>
2019-01-04 23:26:25 +0000

    Added App Context and Auth to App Context.

--- src/components/AppContainer.js
+++ src/components/AppContainer.js
@@ -1,16 +1,45 @@
 import React from 'react';
 import { MuiThemeProvider } from '@material-ui/core/styles';
 import { light } from '../themes';
+import { AppContext } from '../contexts';
 import { HashRouter } from 'react-router-dom';
 import App from './App';
+import { axios } from '../services';
 
 class AppContainer extends React.Component {
+  constructor(props){
+    super(props);
+
+    this.state = {
+      //Auth
+      isAuth: localStorage.getItem('token') !== null
+    };
+  }
+
+  /**
+   * Set authenticated user token and auth state or remove them, clearing the store
+   */
+  setAuth = (value, token) => {
+    if(value === true){
+      localStorage.setItem('token', token);
+      axios.defaults.headers.common['Authorization'] = 'Bearer ' + token;
+  
+      this.setState({isAuth: true});
+    }
+    else {
+      localStorage.removeItem('token');
+      axios.defaults.headers.common['Authorization'] = '';
+    }
+  }
+
   render() {
     return (
       <MuiThemeProvider theme={light}>
-        <HashRouter>
-          <App/>
-        </HashRouter>
+        <AppContext.Provider value={this.state}>
+          <HashRouter>
+            <App/>
+          </HashRouter>
+        </AppContext.Provider>
       </MuiThemeProvider>
     );
   }
---
+++ src/contexts/App.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const context = React.createContext({
+
+});
+
+context.displayName = 'AppContext';
+
+export default context;
